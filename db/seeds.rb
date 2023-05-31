04f76500de194194f3c55d774143b161afb98f3a
Ruben Mota <rubenlisboa95@gmail.com>
2023-05-31 16:01:13 +0100

    Added a Working Simple Airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+
+5.times do
+  Flat.create!(
+    name: Faker::Commerce.department,
+    address: Faker::Address.full_address,
+    description: Faker::GreekPhilosophers.quote,
+    price_per_night: rand(50..200),
+    number_of_guests: rand(1..5)
+  )
+end
